90219fa73d6f4167ae1ca414af3bc503519da728
Titus Bridgwood <titus.bridgwood@dunelm.com>
2024-01-22 10:34:29 +0000

    Add basic visitor trait and impl for ast

--- src/ast.rs
+++ src/ast.rs
@@ -16,4 +16,20 @@ enum Expr {
     Literal(Literal),
 }
 
+pub trait Visitor<T> {
+    fn visit_binary(&mut self, left: &Expr, operator: &Token, right: &Expr) -> T;
+    fn visit_unary(&mut self, operator: &Token, right: &Expr) -> T;
+    fn visit_grouping(&mut self, expr: &Expr) -> T;
+    fn visit_literal(&mut self, literal: &Literal) -> T;
+}
 
+impl Expr {
+    pub fn accept<T>(&self, visitor: &Visitor<T>) -> T {
+        match self {
+            Expr::Binary { left, operator, right } => visitor.visit_binary(left, operator, right),
+            Expr::Unary { operator, right } => visitor.visit_unary(operator, right),
+            Expr::Grouping { expr } => visitor.visit_grouping(expr),
+            Expr::Literal { literal } => visitor.visit_grouping(literal),
+        }
+    }
+}
